71e0500468809da2ab3bab3313e4c9f4f5521d31
Gene Wong <genewong2016@gmail.com>
2023-12-11 15:59:37 +0800

    Initial commit

---
+++ automate-booking-part2.py
@@ -0,0 +1,63 @@
+import os
+import subprocess
+
+import pandas as pd
+from openpyxl import load_workbook
+from openpyxl import Workbook
+from openpyxl.utils import column_index_from_string
+from openpyxl.utils.dataframe import dataframe_to_rows
+import configparser
+
+# Create a configuration object
+config = configparser.ConfigParser()
+
+# Read the configuration file
+config.read('automation_configurations_booking.cfg')
+
+# Retrieve the values from the 'DEFAULT' section
+download_path = config['DEFAULT']['download_path']
+from_to_timestrings = config['DEFAULT']['from_to_datestrings']
+
+# Print the retrieved values
+print("Download Path:", download_path)
+print("From-to Timestrings:", from_to_timestrings)
+
+# Read
+# download_path = '/Users/gw/Downloads/'
+from_to_timestrings = '20230101 20230630'
+output_file_path = download_path + 'tasan to zoho booking import ' + from_to_timestrings + '.xlsx'
+
+reloaded_output_file_path = output_file_path
+reloaded = pd.read_excel(reloaded_output_file_path, usecols=None, engine="openpyxl", dtype=str)
+
+# Create a new workbook
+output_wb = Workbook()
+output_ws = output_wb.active
+
+header_row = ['Journal Number Prefix', 'Journal Number Suffix', 'Journal Date', 'Status', 'Journal Type', 'Journal Transaction Type', 'Reference Number', 'Notes', 'Exchange Rate', 'Tax Name', 'Tax Percentage', 'Tax Type', 'Project Name', 'Debit', 'Credit', 'Account', 'Contact Name', 'Currency', 'Description']
+#, '', '', 'Journal Number Prefix', 'Journal Number Suffix', 'Journal Date', 'Status', 'Journal Type', 'Journal Transaction Type', 'Reference Number', 'Notes', 'Exchange Rate', 'Tax Name', 'Tax Percentage', 'Tax Type', 'Project Name', 'Debit', 'Credit', 'Account	Contact Name', 'Currency', 'Description'] # Replace with the actual header values
+
+																																					
+
+
+output_ws.append(header_row)
+
+# Get the column index of column AA
+start_column = column_index_from_string('AA')
+end_column = column_index_from_string('AT')
+
+# Copy the values from the template (excluding the header) to the new workbook
+# Iterate over the rows of the DataFrame without formulas
+for row in reloaded.itertuples(): # index=False, name=None): # ???
+    values_to_copy = row[start_column:end_column]  # Slice from the start column onwards
+    
+    output_ws.append(values_to_copy)
+
+# for row in reloaded.iter_rows(values_only=True):
+#     # Copy values from column AA and beyond
+#     values_to_copy = row[column_index_from_string('AA')-1:]
+#     output_ws.append(values_to_copy)
+
+# Save the new workbook with values only
+output_values_file_path = download_path + 'tasan to zoho booking import content.xls'
+output_wb.save(output_values_file_path)
---
+++ automate-booking.py
@@ -0,0 +1,62 @@
+import os
+import subprocess
+
+import pandas as pd
+from openpyxl import load_workbook
+from openpyxl import Workbook
+from openpyxl.utils import column_index_from_string
+from openpyxl.utils.dataframe import dataframe_to_rows
+
+import configparser
+
+# Create a new configuration object
+config = configparser.ConfigParser()
+
+# Set path variables
+download_path = '/Users/gw/Downloads/'
+date_path = '20230101 20230630'
+
+# Set the values for 'download path' and 'from_to_datestrings'
+config['DEFAULT'] = {
+    'download_path': download_path,
+    'from_to_datestrings': date_path
+}
+
+# Save the configuration to a file
+with open('automation_configurations_booking.cfg', 'w') as config_file:
+    config.write(config_file)
+
+file_path = download_path + 'tasan booking ' + date_path + '.xls'
+# subprocess.call(['open', file_path])  # Open the file using the default application
+
+# Load the downloaded Excel file using pandas
+# This is a html file masquerading as xls.
+df = pd.read_html(file_path)[0]
+
+df.to_excel(download_path+'tasan booking ' + date_path + '.xlsx', index=False)
+
+df = pd.read_excel(download_path + 'tasan booking ' + date_path + '.xlsx')
+
+# Exclude the header and last line with totals
+values_df = df.iloc[7:-1, :]
+
+print(values_df.head())
+
+
+# Load the Excel template using openpyxl
+template_file_path = download_path + 'tasan to zoho booking import.xlsx'
+template_wb = load_workbook(template_file_path)
+template_ws = template_wb.active
+
+# Copy the values to the template starting from the top-left cell
+for row in values_df.iterrows():
+    template_ws.append(list(row[1].values))
+
+# Save the modified template
+output_file_path = download_path + 'tasan to zoho booking import ' + date_path + '.xlsx'
+template_wb.save(output_file_path)
+
+subprocess.call(['open', output_file_path])  # Open the file using the default application
+
+# Prompt the user for input to interrupt the script
+input("Please make any changes necessary, save on Excel, and exit. Press Enter to continue...")
